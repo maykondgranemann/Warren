76e9d09273d29471bed32f356d97ab56c70221a6
Maykon Granemann <maykondgranemann@gmail.com>
2022-08-25 14:00:41 -0300

    Criando exercicio A pedido do professor

---
+++ Dojo2508/Dojo2508/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dojo2508
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Seje Bem Vindo");
+            System.Console.ReadLine();
+        }
+    }
+}
