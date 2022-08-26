67a43b7df586f01e45e6789825efab6d69e09dfa
Maykon Granemann <maykondgranemann@gmail.com>
2022-08-26 13:47:28 -0300

    Api

---
+++ Warren/Data/Repository/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseModel
+    {
+        public string Create(T model)
+        {
+            return "Criado";
+        }
+        public string Delete(int id)
+        {
+            return "Deletado";
+        }
+
+        public List<T> GetAll()
+        {
+            List<T> list = new List<T>();
+            return list;
+        }
+
+        public T GetById(int id)
+        {
+            T model = null; 
+            return model;
+        }
+
+        public string Update(T model)
+        {
+            return "Alterado";
+        }
+    }
+}
---
+++ Warren/WebApi/Controllers/AnimalController.cs
@@ -0,0 +1,25 @@
+﻿using Data.Model;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        [HttpGet]
+        public Animal Get()
+        {
+            Animal a1 = new Animal { Id = 1, Especie = "Catchoro", Nome = "Chimba", Peso = 3.8f };
+            return a1;
+        }
+        [HttpPost]
+        public string Post(Animal model)
+        {
+            AnimalRepository repo = new AnimalRepository();
+            return repo.Create(model);
+        }
+    }
+}
